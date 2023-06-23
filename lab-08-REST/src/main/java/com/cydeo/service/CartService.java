b1d6af75966e41dcc8b929b037cbce63285e6f27
Ivan_Cydeo <remezivan1@gmail.com>
2023-06-23 18:03:28 -0400

    ECommerce App - CartServiceImpl review done

--- lab-08-REST/src/main/java/com/cydeo/service/CartService.java
+++ lab-08-REST/src/main/java/com/cydeo/service/CartService.java
@@ -1,6 +1,22 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.CartDTO;
+import com.cydeo.entity.Cart;
+import com.cydeo.entity.CartItem;
+import com.cydeo.entity.Customer;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface CartService {
     boolean existById(Long cartId);
+
+    CartDTO findById(Long id);
+
+    boolean addToCart(Customer customer, Long productId, Integer quantity);
+
+    BigDecimal applyDiscountToCartIfApplicableAndCalculateDiscountAmount(String discountName, Cart cart);
+
+    BigDecimal calculateTotalCartAmount(List<CartItem> cartItemList);
 
 }
